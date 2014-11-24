8dd5b951b3ea5c96acefddcbd6ca08e4f49b9d85
werriq <werriqpeterpan@ymail.com>
2014-11-24 18:49:26 +0100

    kla

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ var app = {
     },
     // Bind Event Listeners
     //
+	
     // Bind any events that are required on startup. Common events are:
     // `load`, `deviceready`, `offline`, and `online`.
     bindEvents: function() {
